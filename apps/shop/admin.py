7109029f716aed6e95326ec30e7907d998fcdb8b
gglsilva <gabrielchamex@gmail.com>
2023-04-24 08:09:51 -0300

    Adiciona modificações no admin product e core dashboard

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -13,4 +13,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
+    search_fields = ['name', 'created', 'price']
     prepopulated_fields = {'slug': ('name',)}
